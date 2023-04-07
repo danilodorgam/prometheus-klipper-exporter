471f9a83c00b4b89b68ada93d68a87e8e15ddc8c
Danilo Dorgam <3344991+danilodorgam@users.noreply.github.com>
2023-04-07 20:02:44 -0300

    add entity

--- collector/history.go
+++ collector/history.go
@@ -22,6 +22,44 @@ type MoonrakerHistoryResponse struct {
 	} `json:"result"`
 }
 
+type MoonrakerHistoryCurrentPrintResponse struct {
+	Result struct {
+		Count int `json:"count"`
+		Jobs  []struct {
+			EndTime      float64 `json:"end_time"`
+			FilamentUsed float64 `json:"filament_used"`
+			Filename     string  `json:"filename"`
+			Metadata     struct {
+				Size             int     `json:"size"`
+				Modified         float64 `json:"modified"`
+				Slicer           string  `json:"slicer"`
+				SlicerVersion    string  `json:"slicer_version"`
+				LayerHeight      float64 `json:"layer_height"`
+				FirstLayerHeight float64 `json:"first_layer_height"`
+				ObjectHeight     float64 `json:"object_height"`
+				FilamentTotal    float64 `json:"filament_total"`
+				EstimatedTime    float64 `json:"estimated_time"`
+				Thumbnails       []struct {
+					Width        int    `json:"width"`
+					Height       int    `json:"height"`
+					Size         int    `json:"size"`
+					RelativePath string `json:"relative_path"`
+				} `json:"thumbnails"`
+				FirstLayerBedTemp  float64 `json:"first_layer_bed_temp"`
+				FirstLayerExtrTemp float64 `json:"first_layer_extr_temp"`
+				GcodeStartByte     int     `json:"gcode_start_byte"`
+				GcodeEndByte       int     `json:"gcode_end_byte"`
+			} `json:"metadata"`
+			PrintDuration float64 `json:"print_duration"`
+			Status        string  `json:"status"`
+			StartTime     float64 `json:"start_time"`
+			TotalDuration float64 `json:"total_duration"`
+			JobID         string  `json:"job_id"`
+			Exists        bool    `json:"exists"`
+		} `json:"jobs"`
+	} `json:"result"`
+}
+
 func (c collector) fetchMoonrakerHistory(klipperHost string, apiKey string) (*MoonrakerHistoryResponse, error) {
 	var url = "http://" + klipperHost + "/server/history/totals"
 	c.logger.Debug("Collecting metrics from " + url)
